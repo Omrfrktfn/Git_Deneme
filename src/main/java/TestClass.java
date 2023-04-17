11cd34cf9bb5a25c96afefe305ee3bcd70e35f5a
Omrftktfn <130664507+Omrftktfn@users.noreply.github.com>
2023-04-17 17:54:01 +0200

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,5 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("Git ten merhaba");
+        
+        System.out.println("Git Hub tan merhaba");
     }
 }
